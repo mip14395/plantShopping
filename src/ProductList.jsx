e136994a9843298bbcbf941bf0074815f7b2899c
mip14395 <thtrang14395@gmail.com>
2025-03-05 11:34:38 +0700

    fix

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,7 +292,7 @@ function ProductList() {
       <div className="navbar" style={styleObj}>
         <div className="tag">
           <div className="luxury">
-            <a href="/">
+            <a href="./">
               <img
                 src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png"
                 alt=""
